6e83d0a817a29ae7dff773a6a95d63d941ded16b
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-03 09:06:40 +0530

    [Arti Dandge] Add : Create Person details data

---
+++ AddressBookSystem/PersonDetails.cs
@@ -0,0 +1,39 @@
+﻿
+namespace AddressBookSystem
+{
+    public class PersonDetails
+    {
+        private string first_name;
+        private string last_name;
+        private string address;
+        private string city;
+        private string state;
+        private int zip;
+        private long phone_number;
+        private string email;
+
+        public PersonDetails(string first_name, string last_name, string address, string city, string state, int zip, long phone_number, string email)
+        {
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phone_number = phone_number;
+            this.email = email;
+        }
+
+        public PersonDetails(): this("", "", "", "", "", 0, 0, "")
+        {
+            
+        }
+
+        public string toString()
+        {
+
+            return "Person Details: \tFirst name:" + this.first_name + " \tLast name:" + this.last_name + " \t Address:" + this.address + " \tCity:" + this.city + " \tState:"
+                    + this.state + " \tZip:" + this.zip + " \tPhone Number:" + this.phone_number + " \tEmail:" + this.email;
+        }
+    }
+}
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -7,6 +7,7 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book System !");
+            PersonDetails person = new PersonDetails();
         }
     }
 }
